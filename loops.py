120f8db89ef72cd2998f0017107e527c050b2a08
mutainewton <mutainewton6@gmail.com>
2021-11-20 15:03:26 +0300

    add

---
+++ loops.py
@@ -0,0 +1,26 @@
+i = 0
+while i < 6:
+    print(i)
+    if i == 3:
+        break
+    i=i+1
+
+# continue statementn in while loop
+print("==========")
+i = 0
+while i< 6:
+    i = i+1
+    if i == 3:
+        continue
+    print(i)
+#printing individual characters
+print("==================")
+x = 0
+name = "Tommy Lee"
+while x < 9:
+    print(name[x])
+    x= x+1
+
+
+
+
