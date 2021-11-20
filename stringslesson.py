7e2dfef41a9ef1793c312b1b62ffed1d7ff6dc04
mutainewton <mutainewton6@gmail.com>
2021-11-20 15:03:49 +0300

    add

---
+++ stringslesson.py
@@ -0,0 +1,38 @@
+first_name = "James "
+last_name = 'Antony'
+print(first_name)
+print(last_name)
+
+
+# multiline strings
+lorem = """
+Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+ Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+  when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+   It has survived not only five centuries, but also the leap into electronic typesetting
+   , remaining essentially unchanged. It was popularised in the 1960s withthe release of Letraset sheets containing Lorem Ipsum passages, 
+and more recently with desktop publishing software like Aldus PageMaker
+ including versions of Lorem Ipsum.
+ 
+"""
+print(lorem)
+
+welcome = "Hello world"
+print(welcome [0])
+
+print(welcome[6])
+# slicing
+print(welcome[2:5])
+
+#negative indexing
+print(welcome[-5:-1])
+#string lenght
+mylenght = len(welcome)
+print(mylenght)
+print(len(lorem))
+
+print(lorem [404:409])
+
+
+
+
