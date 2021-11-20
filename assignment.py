25b32c128de8c856cd95fbbefe9423622960a419
mutainewton <mutainewton6@gmail.com>
2021-11-20 14:49:00 +0300

    add

---
+++ assignment.py
@@ -0,0 +1,17 @@
+from math import*
+#Finding initial speed
+
+a = 9
+v = 20
+s = 100
+x = pow(v,2) - 2*a*s
+print(x)
+
+
+# Simple intrest
+p = 50000
+r = 2
+t = 3/12
+
+simpleintrest=(p*r*t)/100
+print("simple intrest:",simpleintrest)
