e881c0c022f0d63feda645eaef022954fc0d5e63
mutainewton <mutainewton6@gmail.com>
2021-11-20 15:02:41 +0300

    add

---
+++ lessonone.py
@@ -0,0 +1,74 @@
+age = 67 #integeter declaration
+salary = 1456.8 # Float declaration
+name = "John" #  Name declaration
+
+print(age) # printing on item
+print(salary)
+print(name)
+
+
+Number = 100
+print("Before declare:",Number)
+Number = 120.3
+print("Number after re-declare:",Number)
+
+# Python Numbers
+x = 1.899
+y = 34567893456
+z =" -3456"
+print(type(x))
+print(type(y))
+print(type(z))
+
+#Arithmetic operations
+a = 10
+b = 45
+c = a+b
+print("Result = ",c)
+
+d = 500
+e = 3
+q = d%e
+print("Modulus of {}%{} is {}" .format(d,e,q))
+
+# find average
+mark1 = 50
+mark2 = 65
+average = (mark1+mark2)/2
+print("Average is :",average)
+
+
+#circumfrence of a circle
+pie = 3.142
+d = 14
+circumfrence = pie*d
+print("Circumfrence is:", circumfrence)
+
+
+# volume of cylinder
+pie = 3.142
+r = 14/100
+h = 1
+
+volume = pie* (r*r)* h
+print("volume is:",volume)
+
+#Bmi
+w = 100
+h = 1.7
+
+Bmi = w/(h*h)
+print("Bmi is:",Bmi)
+
+bmi = w/pow(h,2)
+print("Bmi with power function:",bmi)
+
+#Initial  speed
+V = 20
+a = 9
+s = 100
+
+Initial  =(u2)=(20*20)-s*a*s
+print    ("Initial is:",Initial)
+
+
